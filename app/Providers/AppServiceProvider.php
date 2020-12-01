7273224c160eeb09fb8a3366db0f227f774a1dd4
Muhammad Waqar <mwaqar666@yahoo.com>
2020-12-01 23:14:36 +0500

    foreign key error fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,17 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    final public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+    }
+
+    final public function register(): void
     {
         //
     }
